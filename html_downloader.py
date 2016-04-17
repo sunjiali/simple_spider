038b07e160ea428d2e145e579ba217250289bfaa
sunjiali <275261506@qq.com>
2016-04-17 17:39:56 +0800

    The spider test

---
+++ html_downloader.py
@@ -0,0 +1,18 @@
+'''
+Created on 2016-4-17
+
+@author: sunjiali
+'''
+import urllib2
+class HtmlDownloader(object):
+    def download(self,url):
+        if url is None:
+            return None
+        response = urllib2.urlopen(url)
+        
+        if response.getcode() != 200:
+            return None
+        return response.read()
+        
+
+    
